2fe188bb79f74aaa422aaa098ba646b83447cdce
LipeSanmartin <lipesanmartin@hotmail.com>
2023-03-10 17:47:52 -0300

    Initial commit

---
+++ exercicio4.py
@@ -0,0 +1,19 @@
+class Pessoa:
+    def __init__(self, nome, idade, peso, altura):
+        self.nome = nome
+        self.idade = idade
+        self.peso = peso  # em kg
+        self.altura = altura  # em cm
+
+    def envelhecer(self):
+        self.idade += 1
+
+    def engordar(self):
+        self.peso += 1
+
+    def emagrecer(self):
+        self.peso -= 1
+
+    def crescer(self):
+        while self.idade < 21:
+            self.altura += 0.5
