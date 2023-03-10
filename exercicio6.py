8c6627a953ce1b7921886ca22d519714777e405b
LipeSanmartin <lipesanmartin@hotmail.com>
2023-03-10 19:21:21 -0300

    Initial commit

---
+++ exercicio6.py
@@ -0,0 +1,32 @@
+class Televisao:
+    def __init__(self):
+        while True:
+            self.canal = int(input("Insira o canal inicial (0 a 99): "))
+            if 99 > self.canal >= 0:
+                print("Canal:", self.canal)
+                break
+            else:
+                print("Canal inexistente.")
+        self.volume = 50
+        print("Volume:", self.volume)
+
+    def aumentar_volume(self):
+        if self.volume >= 100:
+            print("Volume máximo.")
+        else:
+            self.volume += 1
+            print("Volume:", self.volume)
+
+    def diminuir_volume(self):
+        if self.volume == 0:
+            print("Mudo.")
+        else:
+            self.volume -= 1
+            print("Volume:", self.volume)
+
+
+tv = Televisao()
+for x in range(0, 52):
+    tv.aumentar_volume()
+for x in range(0, 102):
+    tv.diminuir_volume()
