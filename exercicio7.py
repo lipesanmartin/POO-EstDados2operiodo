b643805ec55018c548d8e0dac8c0f81053d39402
LipeSanmartin <lipesanmartin@hotmail.com>
2023-03-25 02:37:33 -0300

    no message

--- exercicio7.py
+++ exercicio7.py
@@ -1,32 +1,42 @@
 class BichinhoVirtual:
     def __init__(self):
         self.nome = "Sem nome"
-        self.fome = False
+        self.fome = 100
         self.saude = 100
         self.idade = 0
 
-    def alterar_nome(self):
+    def set_nome(self):
         self.nome = str(input("Insira o nome do bichinho virtual: "))
         print(f"O novo nome é {self.nome}")
 
     def perder_saude(self):
-        self.saude -= 1
+        self.saude -= 10
         print("Saúde atual:", self.saude)
 
-    def alterar_fome(self):
-        if self.saude < 60:
-            self.fome = True
-            print("Estou com fome.")
+    def perder_fome(self):
+        self.fome -= 10
+        print("Estou com fome.")
+        self.perder_saude()
 
     def alimentar(self):
-        self.fome = False
-        self.saude = 100
-        print("Alimentado", "Saúde: 100")
+        if self.fome >= 100:
+            print("Fome: ", self.fome)
+        else:
+            self.fome += 10
+            print(f"Alimentado.\nFome: {self.fome}")
 
     def aniversario(self):
         self.idade += 1
         print(f"Feliz aniversário! Agora seu bichinho tem {self.idade} anos!")
 
+    def get_nome(self):
+        return self.nome
 
+    def get_fome(self):
+        return f"Fome: {self.fome}"
 
+    def get_saude(self):
+        return f"Saúde: {self.saude}"
 
+    def get_idade(self):
+        return f"{self.nome} tem {self.idade} ano(s)."
